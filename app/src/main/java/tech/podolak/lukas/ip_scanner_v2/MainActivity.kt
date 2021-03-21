577f7c2bcc5a6dce402b2cd4b3d1e3d7ba53192f
Lukáš Podolák <50804011+lukas-podolak@users.noreply.github.com>
2021-03-21 18:45:49 +0100

    Update

--- app/src/main/java/tech/podolak/lukas/ip_scanner_v2/MainActivity.kt
+++ app/src/main/java/tech/podolak/lukas/ip_scanner_v2/MainActivity.kt
@@ -4,12 +4,12 @@ import android.net.wifi.WifiManager
 import android.os.Bundle
 import android.text.format.Formatter
 import android.util.Log
+import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import java.net.InetAddress
-import java.net.NetworkInterface
+import java.net.*
 import java.util.*
 import kotlin.collections.ArrayList
 
@@ -21,37 +21,40 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         setSupportActionBar(findViewById(R.id.toolbar))
 
-        val testList = generateItemList(25)
-
-        findViewById<RecyclerView>(R.id.recycler_view).adapter = RvAdapter(testList)
-        findViewById<RecyclerView>(R.id.recycler_view).layoutManager = LinearLayoutManager(this)
-        findViewById<RecyclerView>(R.id.recycler_view).setHasFixedSize(true)
 
         val ipAddress = getLocalIpAddress()
         findViewById<TextView>(R.id.ip_text).text = "Your IPv4 address is: $ipAddress"
 
+        val scannerButton: Button = findViewById(R.id.scann_button)
+        scannerButton.setOnClickListener {
+            val ipList = scannNetworkIPs()
+
+            findViewById<RecyclerView>(R.id.recycler_view).adapter = RvAdapter(ipList)
+            findViewById<RecyclerView>(R.id.recycler_view).layoutManager = LinearLayoutManager(this)
+            findViewById<RecyclerView>(R.id.recycler_view).setHasFixedSize(true)
+        }
+    }
+
+    fun scannNetworkIPs(): List<RvItem> {
+        val wm: WifiManager = applicationContext.getSystemService(WIFI_SERVICE) as WifiManager
+        val splitIP = Formatter.formatIpAddress(wm.connectionInfo.ipAddress).split('.').toTypedArray()
+        val list = ArrayList<RvItem>()
+
+        for (i in 1 .. 255) {
+            val ip = splitIP[0] + "." + splitIP[1] + "." + splitIP[2] + "." + i
+
+            list += if (Socket(ip, 7).isBound) {
+                RvItem(R.drawable.ic_check, ip, "This IP Address is available.")
+            } else {
+                RvItem(R.drawable.ic_using, ip, "This IP Address is not available.")
+            }
+        }
+
+        return list
     }
 
     fun getLocalIpAddress(): String? {
         val wm: WifiManager = applicationContext.getSystemService(WIFI_SERVICE) as WifiManager
-
         return Formatter.formatIpAddress(wm.connectionInfo.ipAddress)
-    }
-
-    private fun generateItemList(size: Int): List<RvItem> {
-        val list = ArrayList<RvItem>()
-
-        for (i in 0 until size) {
-            val drawable = when (i % 3) {
-                0 -> R.drawable.ic_check
-                1 -> R.drawable.ic_using
-                else -> R.drawable.ic_other
-            }
-
-            val item = RvItem(drawable, "Item $i", "Random second line bro.")
-            list += item
-        }
-
-        return list
     }
 }
